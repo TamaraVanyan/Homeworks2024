9bf9f199399b2a92731720ee9ad2ed70d92526e6
TamaraVanyan <tam.vanyan@gmail.com>
2024-09-13 00:17:33 +0400

    Adding tasks about Arrays

---
+++ TasksAboutArrays/ArraySizeWithN.java
@@ -0,0 +1,37 @@
+package TasksAboutArrays;
+import java.util.Scanner;
+public class ArraySizeWithN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if(n == 0 || n < 0){
+            System.out.println("n should be natural number.");
+            System.exit(2);
+        }
+
+        int[] array;
+        switch (n) {
+            case 3:
+                array = new int[3];
+                break;
+            case 4:
+                array = new int[4];
+                break;
+            case 5:
+                array = new int[5];
+                break;
+            case 6:
+            case 7:
+                array = new int[10];
+                break;
+            default:
+                array = new int[15];
+                array[0] = 15;
+                System.out.println("Array firs element is: " + array[0]);
+                break;
+        }
+        System.out.println("Array size: " + array.length);
+    }
+}
